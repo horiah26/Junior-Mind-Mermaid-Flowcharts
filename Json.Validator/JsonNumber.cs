285a4aa50e82f67a2ed79ef37d16d1766a09116c
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-05-04 17:50:40 +0300

    Replaced Json Number FIles

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -6,34 +6,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            if (string.IsNullOrEmpty(input))
-            {
-                return false;
-            }
-
-            if (MultipleDots(input))
-            {
-                return false;
-            }
-
-            if (input[0] == '0' && input.Length != 1 && input.IndexOf(".") != 1)
-            {
-                return false;
-            }
-
-            if (input[input.Length - 1] == '.')
-            {
-                return false;
-            }
-
-            double number;
-            return double.TryParse(input, out number);
-        }
-
-        static bool MultipleDots(string input)
-        {
-            const int maxDots = 2;
-            return input.Length - input.Replace(".", "").Length >= maxDots;
+            throw new NotImplementedException();
         }
     }
 }
