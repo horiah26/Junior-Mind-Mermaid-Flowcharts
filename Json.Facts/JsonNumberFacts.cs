9f918468050dd1b7d61b58814eb9c667c85faf86
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-05-05 10:28:39 +0300

    Exponent does not allow letters

--- Json.Facts/JsonNumberFacts.cs
+++ Json.Facts/JsonNumberFacts.cs
@@ -121,7 +121,7 @@ namespace Json.Facts
             Assert.True(IsJsonNumber("12.34E3"));
         }
 
-        [Fact(Skip = "Remove this Skip as you implement")]
+        [Fact]
         public void TheExponentDoesNotAllowLetters()
         {
             Assert.False(IsJsonNumber("22e3x3"));
