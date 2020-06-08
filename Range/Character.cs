1a5660634ec2e0d61d69a363259c4c5a2a881f47
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-06-08 12:33:41 +0300

    Added Character class

---
+++ Range/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Range
+{
+    class Character
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            return text[0] == pattern;
+        }
+    }
+
+}
