9585eaee6a80e7d4df5e324f0961258e57f723d3
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-06-23 12:48:56 +0300

    Interfaces in their own files

---
+++ JSON/IMatch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON
+{
+    public interface IMatch
+    {
+        bool Success();
+        string RemainingText();
+    }
+}
--- JSON/IPattern.cs
+++ JSON/IPattern.cs
@@ -1,21 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace JSON
 {
-    public interface IMatch
-    {
-        bool Success();
-        string RemainingText();
-    }
-
     public interface IPattern
     {
         IMatch Match(string text);
-    }
-
-    class Interfaces
-    {
     }
 }
