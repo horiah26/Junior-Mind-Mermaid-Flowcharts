b8dad79eb758c9312319a4bb772634954233177d
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-07-02 14:26:42 +0300

    Added Value Class

---
+++ JSON/Value.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON
+{
+    public class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var whitespace = new Any(" \n\r\t") ;
+
+            var value = new Choice(new String(), 
+                                   new Number(), 
+                                   new Text("null"), 
+                                   new Text("true"), 
+                                   new Text("false"));
+
+            var objValue = new Sequence(whitespace, 
+                                        new String(), 
+                                        whitespace, 
+                                        new Character(':'), 
+                                        value);
+
+            var objList = new List(objValue, new Character(','));
+
+            var obj = new Sequence(new Character('{'), 
+                                   new Choice (objList, whitespace), 
+                                   new Character('}'));
+
+            var arrayList = new List(value, new Character(','));
+
+            var array = new Sequence(new Character('['), 
+                                     new Choice(arrayList, whitespace), 
+                                     new Character(']'));
+
+            value.Add(obj);
+            value.Add(array);
+
+            pattern = value;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+
+}
