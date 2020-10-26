33e3238d3fd00b8198dcb3d4405c301be78b6215
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-10-26 13:11:39 +0200

    ThenBy v3

--- LINQ/LINQ/LINQ/Program.cs
+++ LINQ/LINQ/LINQ/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace LINQ
+namespace newspace
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            var stock = new Stock();
-            var product1 = new Product("Product 1", 10);
-            var product2 = new Product("Product 2", 7);
-
-            stock.AddProduct(product1);
-            stock.AddProduct(product2);
-            stock.Substract("Product 1", 1);
-            stock.Substract("Product 2", 1);
-            Console.WriteLine(stock.TotalQuantity());
-
         }
     }
 }
--- LINQ/LINQ/LINQ/ThenBy.cs
+++ LINQ/LINQ/LINQ/ThenBy.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using System.Diagnostics.CodeAnalysis;
 
 namespace LINQ
 {
-    class ThenBy
+    static class ThenByClass
     {
-
         public static IOrderedEnumerable<TSource> ThenBy<TSource, TKey>(
             this IOrderedEnumerable<TSource> source,
             Func<TSource, TKey> keySelector,
             IComparer<TKey> comparer)
-        {
+        {            
+            var linkerComparer = new LinkerComparer<TSource, TKey>(new List<IComparer<TSource>>());
+            var customSource = new CustomOrderedEnumerable<TSource, TKey>(source, linkerComparer);
+            var newComparer = new CustomComparer<TSource, TKey>(keySelector, comparer);
 
-
+            return customSource.CreateCustomOrderedEnumerable(new LinkerComparer<TSource, TKey>(linkerComparer, newComparer));
         }
 
         public class CustomComparer<TSource, TKey> : IComparer<TSource>
@@ -37,24 +38,96 @@ namespace LINQ
 
         public class LinkerComparer<TSource, TKey> : IComparer<TSource>
         {
-            IOrderedEnumerable<IComparer<TSource>> comparers;
+            List<IComparer<TSource>> comparers;
 
-            public LinkerComparer(IOrderedEnumerable<IComparer<TSource>> comparers)
+            public LinkerComparer(List<IComparer<TSource>> comparers)
             {
                 this.comparers = comparers;
             }
 
+            public LinkerComparer(LinkerComparer<TSource, TKey> linkerComparer, CustomComparer<TSource,TKey> newComparer)
+            {
+                comparers = linkerComparer.comparers;
+                comparers.Add(newComparer);
+            }
+
             public int Compare([AllowNull] TSource x, [AllowNull] TSource y)
             {
-                var comparerEnum = comparers.GetEnumerator();
+                int comparerResult = 0;
 
-                while (comparerEnum.MoveNext() && comparerEnum.Current.Compare(x, y) == 0) { }
+                foreach (var comparer in comparers)
+                {
+                    comparerResult = comparer.Compare(x, y);
 
-                comparerEnum.MoveNext();
+                    if (comparerResult != 0)
+                    {
+                        return comparerResult;
+                    }
+                }
 
-                return comparerEnum.Current.Compare(x, y);
+                return comparerResult;
             }
         }
 
+        private class CustomOrderedEnumerable<TSource, TKey> : IOrderedEnumerable<TSource>
+        {
+            private readonly IEnumerable<TSource> source;
+            private readonly LinkerComparer<TSource, TKey> linkerComparer;
+
+            public CustomOrderedEnumerable(IEnumerable<TSource> source, LinkerComparer<TSource, TKey> linkerComparer)
+            {
+                this.source = source;
+                this.linkerComparer = linkerComparer;
+            }
+
+            public IOrderedEnumerable<TSource> CreateCustomOrderedEnumerable<TKey>(LinkerComparer<TSource, TKey> linkComparer)
+            {
+                return new CustomOrderedEnumerable<TSource, TKey>(source, linkComparer);
+            }
+
+            public IEnumerator<TSource> GetEnumerator()
+            {
+                var list = new List<TSource>() {};
+
+                foreach (var element in source)
+                {
+                    int i = 0;
+                    bool inserted = false;
+
+                    foreach (var item in list)
+                    {
+                        if (linkerComparer.Compare(element, item) == -1)
+                        {
+                            list.Insert(i, element);
+                            inserted = true;
+                        }
+
+                        i++;
+                    }
+
+                    if (!inserted)
+                    {
+                        list.Add(element);
+                    }
+                }
+
+                foreach (var item in list)
+                {
+                    yield return item;
+                }
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public CustomOrderedEnumerable(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer) { }
+
+            public IOrderedEnumerable<TSource> CreateOrderedEnumerable<TKey>(Func<TSource, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+            {
+                return new CustomOrderedEnumerable<TSource, TKey>(source, keySelector, comparer);
+            }
+        }
     }
 }
