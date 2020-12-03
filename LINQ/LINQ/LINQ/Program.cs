e07511ff0a0acc8e9757f6261bd772e403377caf
horiah26 <57143792+horiah26@users.noreply.github.com>
2020-12-03 10:35:59 +0200

    Added Flowcharts

--- LINQ/LINQ/LINQ/Program.cs
+++ LINQ/LINQ/LINQ/Program.cs
@@ -8,6 +8,14 @@ namespace LINQ
     {
         public static void Main()
         {
+            List<int> lista = new List<int> { 1, 2, 3, 4, 5};
+            var a = lista.SkipLast(2);
+            var b = lista.TakeLast(2);
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
