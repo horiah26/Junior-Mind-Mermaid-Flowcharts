3e56c668caf34759c483c71b900958554dd79961
horiah26 <57143792+horiah26@users.noreply.github.com>
2021-02-26 14:10:04 +0200

    ShapeParallelogramAlt done

--- Flowcharts/Flowcharts/Program.cs
+++ Flowcharts/Flowcharts/Program.cs
@@ -7,19 +7,19 @@
             string fileName = "test";
             string path = @"D:\Code\OOP2\Flowcharts\Flowcharts\bin\Debug\";
 
-            var flowchart = new Flowchart("DownTop", fileName, path);
+            var flowchart = new Flowchart("RightLeft", fileName, path);
 
-            flowchart.AddPair(("A", "Parallelogram"), ("Parallelogram das fasf asfa sfafsd fsdfa dsafgwry wehdfffaf", "Parallelogram"));
-            flowchart.AddPair(("AB", "Parallelogram"), ("Parallelogram 3", "Parallelogram"));
-            flowchart.AddPair(("ABC", "Parallelogram"), ("Rhombh dfh dhdsfhth dgs hgdus 4", "Parallelogram"));
-            flowchart.AddPair(("ABCD", "Parallelogram"), ("Parallelograms 4", "Parallelogram"));
-            flowchart.AddPair(("ABCDEF", "Parallelogram"), ("Rhombjhd jhdss gfhnygtikyilgjgdf dxgfh dtju fjsus d4", "Parallelogram"));
-            flowchart.AddPair(("ABCDEFG", "Parallelogram"), ("Circldsge hsdah sah 5etrs hdrs gs  df4", "Parallelogram"));
-            flowchart.AddPair(("abcdefg", "Parallelogram"), ("Circle hsdah sahasg 5etrs hdrs gs  df4", "Parallelogram"));
-            flowchart.AddPair(("ABCDda da sdas avc sdfafa sdfcawda sfdas afEFGHI", "Parallelogram"), ("Circle hsdagaah sah 5etrs hdrs gs  df4", "Parallelogram"));
-            flowchart.AddPair(("ABCDEfasf a afasf saf sagvfsdgvree bs bsvb s aFGHIJ", "Parallelogram"), ("Circle hsdaaah sah 5etrs hdrs gs  df4", "Parallelogram"));
-            flowchart.AddPair(("ABCDsdg a dsgrsgad vsdfg wegasv dsavaEFGHIJK", "Parallelogram"), ("Circle hsdah sah agag5etrs hdrs gs  df4", "Parallelogram"));
-            flowchart.AddPair(("ABCDEFGHIJKL", "Parallelogram"), ("Circle hasfsdah sah 5etrs hdrs gs  df4", "Parallelogram"));
+            flowchart.AddPair(("A", "ParallelogramAlt"), ("ParallelogramAlt das fasf asfa sfafsd fsdfa dsafgwry wehdfffaf", "ParallelogramAlt"));
+            flowchart.AddPair(("AB", "ParallelogramAlt"), ("ParallelogramAlt 3", "ParallelogramAlt"));
+            flowchart.AddPair(("ABC", "ParallelogramAlt"), ("Rhombh dfh dhdsfhth dgs hgdus 4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCD", "ParallelogramAlt"), ("ParallelogramAlts 4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDEF", "ParallelogramAlt"), ("Rhombjhd jhdss gfhnygtikyilgjgdf dxgfh dtju fjsus d4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDEFG", "ParallelogramAlt"), ("Circldsge hsdah sah 5etrs hdrs gs  df4", "ParallelogramAlt"));
+            flowchart.AddPair(("abcdefg", "ParallelogramAlt"), ("Circle hsdah sahasg 5etrs hdrs gs  df4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDda da sdas avc sdfafa sdfcawda sfdas afEFGHI", "ParallelogramAlt"), ("Circle hsdagaah sah 5etrs hdrs gs  df4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDEfasf a afasf saf sagvfsdgvree bs bsvb s aFGHIJ", "ParallelogramAlt"), ("Circle hsdaaah sah 5etrs hdrs gs  df4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDsdg a dsgrsgad vsdfg wegasv dsavaEFGHIJK", "ParallelogramAlt"), ("Circle hsdah sah agag5etrs hdrs gs  df4", "ParallelogramAlt"));
+            flowchart.AddPair(("ABCDEFGHIJKL", "ParallelogramAlt"), ("Circle hasfsdah sah 5etrs hdrs gs  df4", "ParallelogramAlt"));
 
             flowchart.Draw();
         }
--- Flowcharts/Flowcharts/ShapeParallelogramAlt.cs
+++ Flowcharts/Flowcharts/ShapeParallelogramAlt.cs
@@ -3,11 +3,11 @@ using System.Xml;
 
 namespace Flowcharts
 {
-    class ShapeParallelogram : ShapePolygon
+    class ShapeParallelogramAlt : ShapePolygon
     {
         private readonly double inclination;
 
-        public ShapeParallelogram(XmlWriter xmlWriter, IOrientation orientation, string text) : base(xmlWriter, orientation, text)
+        public ShapeParallelogramAlt(XmlWriter xmlWriter, IOrientation orientation, string text) : base(xmlWriter, orientation, text)
         {
             (string[] lines, _) = new TextSplitter(text).Split();
             inclination = Math.Sqrt(lines.Length) * 15;
@@ -15,7 +15,7 @@ namespace Flowcharts
 
         public override string CalculateCornerPoints()
         {
-            return new ShapeParallelogramPointsCalculator(xPos, yPos, height, length, lines, inclination).Calculate();
+            return new ShapeParallelogramAltPointsCalculator(xPos, yPos, height, length, lines, inclination).Calculate();
         }
 
         public override EntryExitPoints CalculateInOut()
