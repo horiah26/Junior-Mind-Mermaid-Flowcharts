015dc84fc7665ed376c7e4174e589831975b4b1c
horiah26 <57143792+horiah26@users.noreply.github.com>
2021-03-30 11:39:06 +0300

    Text aligned left

--- Flowcharts/Flowcharts/WrittenText.cs
+++ Flowcharts/Flowcharts/WrittenText.cs
@@ -23,38 +23,23 @@ namespace Flowcharts
         {
             int spaceBetweenLines = 17;
 
-            var linePositionList = CalculatePosition(lines);
-            var maxDifference = linePositionList.Max(x => x.gap);
-
             xmlWriter.WriteStartElement("text");
-            xmlWriter.WriteAttributeString("x", (xPosition + maxDifference - linePositionList[0].gap).ToString());
+            xmlWriter.WriteAttributeString("x", (xPosition).ToString());
             xmlWriter.WriteAttributeString("y", yPosition.ToString());
             xmlWriter.WriteAttributeString("fill", "black");
             xmlWriter.WriteString(lines[0]);
 
-            for (int i = 1; i < linePositionList.Count(); i++)
+            for (int i = 1; i < lines.Count(); i++)
             {
                 xmlWriter.WriteStartElement("tspan");
-                xmlWriter.WriteAttributeString("x", (xPosition + maxDifference - linePositionList[i].gap).ToString());
+                xmlWriter.WriteAttributeString("x", (xPosition).ToString());
                 xmlWriter.WriteAttributeString("y", (yPosition + i * spaceBetweenLines).ToString());
-                xmlWriter.WriteString(linePositionList[i].line);
+                xmlWriter.WriteString(lines[i]);
                 xmlWriter.WriteEndElement();
             }
 
             xmlWriter.WriteEndElement();
         }
 
-        public List<(string line, double gap)> CalculatePosition(string[] lines)
-        {
-            List<(string line, double gap)> list = new List<(string line, double gap)>() { };
-
-            foreach (var line in lines)
-            {
-                double gap = new TextSizeCalculator(new string[] { line }).Calculate();
-                list.Add((line, gap));
-            }
-
-            return list;
-        }
     }
 }
